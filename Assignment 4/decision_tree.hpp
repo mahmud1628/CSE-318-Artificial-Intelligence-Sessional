71dfcf057aa7575971e52a863f7ad737f4479940
mahmud1628 <2105120@ugrad.cse.buet.ac.bd>
2025-07-03 00:14:10 +0600

    Function to calculate entropy

--- Assignment 4/decision_tree.hpp
+++ Assignment 4/decision_tree.hpp
@@ -4,6 +4,7 @@
 #include "node.hpp"
 #include <iostream>
 #include <vector>
+#include <cmath>
 using namespace std;
 
 struct DataPoint
@@ -18,6 +19,12 @@ class DecisionTree
     int max_depth;
 
     Node * build_tree(const vector<DataPoint> & data, const vector<string> & attributes, int depth);
+    string get_best_attribute(const vector<DataPoint> & data, const vector<string> & attributes);
+
+
+    // selection criteria
+    double entropy(const vector<DataPoint> & data);
+    double information_gain(const vector<DataPoint> & data, const string & attribute);
 
     public:
     DecisionTree(int max_depth = 5) : root(nullptr), max_depth(max_depth) {}
@@ -37,6 +44,11 @@ bool DecisionTree::train(const vector<DataPoint> & data, const vector<string> & 
         return false;
     }
 
+    this->root = new Node();
+
+    // TODO: get best attribute and set that as root attribute
+
+
     this->root = build_tree(data, attributes, 0); // root node is at depth 0
     if(this->root == nullptr)
     {
@@ -54,6 +66,36 @@ Node * DecisionTree::build_tree(const vector<DataPoint> & data, const vector<str
         // TODO: Find the majority class label in the data here
         return new Node(true, "", majority_class_label, depth);
     }
+}
+
+
+string DecisionTree::get_best_attribute(const vector<DataPoint> & data, const vector<string> & attributes)
+{
+    
+}
+
+
+// selection criteria
+
+double DecisionTree::entropy(const vector<DataPoint> & data)
+{
+    if(data.empty()) return 0.0;
+
+    unordered_map<string, int> class_counts;
+
+    for(const auto & point : data)
+    {
+        class_counts[point.class_label]++;
+    }
+
+    double entropy_value = 0.0;
+    int total_count = data.size();
+    for(const auto & pair : class_counts)
+    {
+        double probability = static_cast<double>(pair.second) / total_count;
+        entropy_value -= probability * log2(probability);
+    }
+    return entropy_value;
 }
 
 
