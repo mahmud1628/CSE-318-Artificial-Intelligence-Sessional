3ee0e55f38fba511ce4a50f1cc77b9a6114e72c0
mahmud1628 <2105120@ugrad.cse.buet.ac.bd>
2025-07-04 08:31:33 +0600

    Train function completed

--- Assignment 4/decision_tree.hpp
+++ Assignment 4/decision_tree.hpp
@@ -23,7 +23,11 @@ class DecisionTree
 
     Node * build_tree(const vector<DataPoint> & data, const vector<string> & attributes, int depth);
     string get_best_attribute(const vector<DataPoint> & data, const vector<string> & attributes);
+    string get_majority_class(const vector<DataPoint> & data);
+    bool is_pure_node(const vector<DataPoint> & data);
 
+
+    void print_tree(Node * node, int depth);
 
     // selection criteria
     double entropy(const vector<DataPoint> & data);
@@ -33,18 +37,9 @@ class DecisionTree
 
     public:
     DecisionTree(int max_depth = 5, string selection_strategy = "ig") : root(nullptr), max_depth(max_depth) {
-        if(selection_strategy == "ig")
-        {
-            attribute_selection_strategy = &DecisionTree::information_gain;
-        }
-        else if(selection_strategy == "igr")
-        {
-            attribute_selection_strategy = &DecisionTree::information_gain_ratio;
-        }
-        else if(selection_strategy == "nwig")
-        {
-            attribute_selection_strategy = &DecisionTree::normalized_weighted_information_gain;
-        }
+        if(selection_strategy == "ig") attribute_selection_strategy = &DecisionTree::information_gain;
+        else if(selection_strategy == "igr") attribute_selection_strategy = &DecisionTree::information_gain_ratio;
+        else if(selection_strategy == "nwig") attribute_selection_strategy = &DecisionTree::normalized_weighted_information_gain;
         else
         {
             cerr << "Error: Unsupported attribute selection strategy." << endl;
@@ -57,6 +52,7 @@ class DecisionTree
     }
 
     bool train(const vector<DataPoint> & data, const vector<string> & attrributes);
+    void print();
 };
 
 bool DecisionTree::train(const vector<DataPoint> & data, const vector<string> & attributes)
@@ -67,31 +63,104 @@ bool DecisionTree::train(const vector<DataPoint> & data, const vector<string> & 
         return false;
     }
 
-    this->root = new Node();
-
-    // TODO: get best attribute and set that as root attribute
-    string best_attribute = get_best_attribute(data, attributes);
-
-    cout << "Best attribute for root node: " << best_attribute << endl;
-
-
-    // this->root = build_tree(data, attributes, 0); // root node is at depth 0
-    // if(this->root == nullptr)
-    // {
-    //     cerr << "Error: Failed to build the decision tree." << endl;
-    //     return false;
-    // }
+    root = build_tree(data, attributes, 0);
+    if(root == nullptr)
+    {
+        cerr << "Error: Failed to build the decision tree." << endl;
+        return false;
+    }
     return true; // Successfully built the tree
 }
 
 Node * DecisionTree::build_tree(const vector<DataPoint> & data, const vector<string> & attributes, int depth)
 {
+    if(data.empty() || attributes.empty())
+    {
+        return nullptr; // No data to build the tree
+    }
     if(depth == this->max_depth) // maximum depth reached
     {
-        string majority_class_label;
-        // TODO: Find the majority class label in the data here
+        string majority_class_label = get_majority_class(data);
         return new Node(true, "", majority_class_label, depth);
     }
+    // Check if all data points belong to the same class,i.e. pure node
+    bool is_pure = is_pure_node(data);
+    if(is_pure)
+    {
+        return new Node(true, "", data[0].class_label, depth);
+    }
+
+    // find the best attribute to split on
+    string best_attribute = get_best_attribute(data, attributes);
+    if(best_attribute.empty()) // no valid attribute found, return majority class
+    {
+        string majority_class_label = get_majority_class(data);
+        return new Node(true, "", majority_class_label, depth);
+    }
+
+    Node * node = new Node(false, best_attribute, "", depth);
+    unordered_set<string> attribute_values;
+    for(const auto & point : data)
+    {
+        attribute_values.insert(point.attribute_values.at(best_attribute));
+    }
+    vector<string> remaining_attributes;
+    for(const auto & attr : attributes)
+    {
+        if(attr != best_attribute)
+        {
+            remaining_attributes.push_back(attr);
+        }
+    }
+    for(const auto & value : attribute_values)
+    {
+        vector<DataPoint> subset;
+        for(const auto & point : data)
+        {
+            if(point.attribute_values.at(best_attribute) == value)
+            {
+                subset.push_back(point); // Collect data points with the current attribute value
+            }
+        }
+        Node * child = build_tree(subset, remaining_attributes, depth + 1);
+        node->children[value] = child;
+    }
+
+    return node;
+}
+
+bool DecisionTree::is_pure_node(const vector<DataPoint> & data)
+{
+    if(data.empty()) return false; // Empty data cannot be pure
+    string first_class = data[0].class_label;
+    for(const auto & point : data)
+    {
+        if(point.class_label != first_class)
+        {
+            return false; // Found a different class label, not pure
+        }
+    }
+    return true; // Didn't find any different class labels, so it's pure
+}
+
+string DecisionTree::get_majority_class(const vector<DataPoint> & data)
+{
+    unordered_map<string, int> class_counts;
+    for(const auto & point : data)
+    {
+        class_counts[point.class_label]++;
+    }
+    string majority_class;
+    int max_count = 0;
+    for(const auto & pair : class_counts)
+    {
+        if(pair.second > max_count)
+        {
+            max_count = pair.second;
+            majority_class = pair.first;
+        }
+    }
+    return majority_class; // Return the class label with the highest count
 }
 
 string DecisionTree::get_best_attribute(const vector<DataPoint> & data, const vector<string> & attributes)
@@ -110,6 +179,9 @@ string DecisionTree::get_best_attribute(const vector<DataPoint> & data, const ve
     cout << "Best value for attribute '" << best_attribute << "': " << best_value << endl;
     return best_attribute;
 }
+
+
+
 
 
 // selection criteria
@@ -194,6 +266,28 @@ double DecisionTree::normalized_weighted_information_gain(const vector<DataPoint
     normalized_gain = information_gain_value * normalized_gain;
     normalized_gain = normalized_gain / log2(k + 1);
     return normalized_gain;
+}
+
+// print
+void DecisionTree::print_tree(Node* node, int tree_depth) 
+{
+    if (node == nullptr) return;
+    
+    std::string indent(tree_depth * 4, ' ');
+    if (node->is_leaf) {
+        std::cout << indent << "-> Predict: " << node->predicted_class << "\n";
+    } else {
+        std::cout << indent << "Split on: " << node->attribute_name << "?\n";
+        for (const auto& pair : node->children) {
+            std::cout << indent << "    - " << pair.first << ":\n";
+            print_tree(pair.second, tree_depth + 1);
+        }
+    }
+}
+
+void DecisionTree::print()
+{
+    print_tree(root, 0);
 }
 
 
